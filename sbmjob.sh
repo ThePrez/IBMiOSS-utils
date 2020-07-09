e19209ec52fc9df5a282859324de1fd6c1246bce
Jesse Gorzinski <jgorzinski@gmail.com>
2020-07-09 14:35:02 -0500

    Add QIBM_USE_DESCRIPTOR_STDIO=N

--- sbmjob.sh
+++ sbmjob.sh
@@ -1,4 +1,6 @@
 #!/QOpenSys/usr/bin/sh
 JOBNAME=$(echo "$1" | sed 's/[^a-zA-Z0-9]//g' | cut -c-10) 
 SBMJOB_OPTS="JOB($JOBNAME) $SBMJOB_OPTS" 
+QIBM_USE_DESCRIPTOR_STDIO=N
+export QIBM_USE_DESCRIPTOR_STDIO
 exec /QOpenSys/usr/bin/system -kpiveO "SBMJOB CMD(CALL PGM(QP2SHELL2) PARM('/QOpenSys/usr/bin/sh' '-c' 'cd $(pwd) && env && exec $*')) CPYENVVAR(*YES) PRTDEV(*USRPRF) ALWMLTTHD(*YES) $SBMJOB_OPTS"
