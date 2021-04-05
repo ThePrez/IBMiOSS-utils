90d6bf2c8a2e9853d169cbdad7e4ad88728d868b
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2021-04-05 09:20:10 -0500

    Update fix_ssh_permissions.sh

--- fix_ssh_permissions.sh
+++ fix_ssh_permissions.sh
@@ -1,5 +1,5 @@
 #!/QOpenSys/usr/bin/sh
-chown -R `whoami` $HOME/.ssh
+chown -R `/QOpenSys/usr/bib/id -u -n` $HOME/.ssh
 chmod 0755 $HOME
 chmod 0700 $HOME/.ssh
 chmod 0644 $HOME/.ssh/authorized_keys
