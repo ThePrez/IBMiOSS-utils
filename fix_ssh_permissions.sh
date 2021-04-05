b6084225471a871c8bf8429f1d69f7ac70873744
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2021-04-05 09:21:30 -0500

    Update fix_ssh_permissions.sh

--- fix_ssh_permissions.sh
+++ fix_ssh_permissions.sh
@@ -1,4 +1,6 @@
 #!/QOpenSys/usr/bin/sh
+# To fix up directory permissions after running ssh-copy-id to copy your keys to IBM i
+# Download and run this script, or copy/paste the following commands into an IBM i shell (preferrably SSH)
 chown -R `/QOpenSys/usr/bib/id -u -n` $HOME/.ssh
 chmod 0755 $HOME
 chmod 0700 $HOME/.ssh
