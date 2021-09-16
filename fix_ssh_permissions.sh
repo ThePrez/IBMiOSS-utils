5daef9f8be758388d25b4cfb54f30f1bbec455fa
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2021-09-16 10:37:00 -0500

    Create and chown home dir

--- fix_ssh_permissions.sh
+++ fix_ssh_permissions.sh
@@ -1,6 +1,8 @@
 #!/QOpenSys/usr/bin/sh
 # To fix up directory permissions after running ssh-copy-id to copy your keys to IBM i
 # Download and run this script, or copy/paste the following commands into an IBM i shell (preferrably SSH)
+mkdir -p $HOME/.ssh
+chown `/QOpenSys/usr/bib/id -u -n` $HOME
 chown -R `/QOpenSys/usr/bib/id -u -n` $HOME/.ssh
 chmod 0755 $HOME
 chmod 0700 $HOME/.ssh
