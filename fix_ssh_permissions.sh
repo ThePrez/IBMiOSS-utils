3b04045db785cbf0a39e55b9bd9eabee33dccf5d
Christian Jorgensen <cj@dk.nmp.eu>
2021-11-08 17:06:01 +0100

    Fix typo and follow shellcheck recommendation for SC2086

--- fix_ssh_permissions.sh
+++ fix_ssh_permissions.sh
@@ -2,8 +2,8 @@
 # To fix up directory permissions after running ssh-copy-id to copy your keys to IBM i
 # Download and run this script, or copy/paste the following commands into an IBM i shell (preferrably SSH)
 mkdir -p $HOME/.ssh
-chown `/QOpenSys/usr/bib/id -u -n` $HOME
-chown -R `/QOpenSys/usr/bib/id -u -n` $HOME/.ssh
+chown "$(/QOpenSys/usr/bin/id -u -n)" $HOME
+chown -R "$(/QOpenSys/usr/bin/id -u -n)" $HOME/.ssh
 chmod 0755 $HOME
 chmod 0700 $HOME/.ssh
 chmod 0644 $HOME/.ssh/authorized_keys
