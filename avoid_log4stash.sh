164f2ea63ee170ef200ee6af8844e6e18e5ca5de
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2021-12-13 11:23:59 -0600

    Update avoid_log4stash.sh

--- avoid_log4stash.sh
+++ avoid_log4stash.sh
@@ -6,9 +6,13 @@
 # - Please see avoid_log4shell.sh for the current version! When I first published, I borked up the name,
 #   but I am leaving this here in case anyone references it. OOPS!!
 #   The properly-named version is at https://github.com/ThePrez/IBMiOSS-utils/blob/master/avoid_log4shell.sh
-#
+# 
+# - This script comes with no form of warranty or guarantee. Please reference below license. 
+# 
 # - This script is intended to serve as a part of a containment and mitigation activity for the
-#   Log4Stash vulnerability. IT IS NOT A COMPLETE FIX!
+#   Log4Stash vulnerability. IT IS NOT A COMPLETE FIX! Some versions of log4j will still be exposed.
+#   See https://www.fastly.com/blog/digging-deeper-into-log4shell-0day-rce-exploit-found-in-log4j
+#   for more details
 # 
 # - This script assumes you are not utilizing JAVA_TOOL_OPTIONS to set Java options today. Also, this
 #   script comes with no warranty (see license for details).
