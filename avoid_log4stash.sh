924d36c95640ccb4911206f73903db3a8172f1ea
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2021-12-13 13:16:56 -0600

    Update avoid_log4stash.sh

--- avoid_log4stash.sh
+++ avoid_log4stash.sh
@@ -58,7 +58,7 @@
 
 
 qsh -c 'touch -C 37 /QIBM/UserData/Java400/SystemDefault.properties'
-echo 'log4j2.formatMsgNoLookups=true' >> /QIBM/UserData/Java400/SystemDefault.properties
+qsh -c 'echo "log4j2.formatMsgNoLookups=true" >> /QIBM/UserData/Java400/SystemDefault.properties'
 
 # in case you're using a non-JV1 Java
 system -kpieb "ADDENVVAR ENVVAR(JAVA_TOOL_OPTIONS) VALUE('-Dlog4j2.formatMsgNoLookups=true') REPLACE(*YES) LEVEL(*SYS)"
